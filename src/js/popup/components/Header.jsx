70563cfb6255c358ef222957501287cb6ea42161
Kevin Shah <kevinshah@Kevins-MacBook-Pro.local>
2020-11-13 02:03:39 -0500

    options page now loads

--- src/js/popup/components/Header.jsx
+++ src/js/popup/components/Header.jsx
@@ -3,12 +3,16 @@ import '../../../css/popup.css'
 import logo from "../../../img/logo.png"
 
 export default class Header extends Component {
+    onOptionsClicked = () => {
+        chrome.runtime.openOptionsPage()
+    }
+
     render() {
         return (
             <div id='header'>
                 <img src={logo} width="150px" height="150px"/>
-                <div id='title'>Price Tracker</div>
-                <button style={{ fontSize: '24px' }} onClick={console.log('options clicked')}>
+                <div className='title'>Price Tracker</div>
+                <button style={{ fontSize: '24px' }} onClick={this.onOptionsClicked}>
                     <i className="fa fa-gear"></i>
                 </button>
             </div>
