3f66019c46c4e84c3fba2017dd10354f88e39bfc
Kevin Shah <kevinshah@Kevins-MacBook-Pro.local>
2020-11-13 01:31:58 -0500

    added login check

--- src/js/popup/Login.jsx
+++ src/js/popup/Login.jsx
@@ -3,11 +3,33 @@ import SignInForm from './components/SignInForm'
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+    state = {
+        error: false,
+    }
+
+    checkCredentials = (email, password) => {
+        if (email === 'test' && password === 'test') {
+            this.setState({
+                error: false,
+            })
+        } else {
+            this.setState({
+                error: true,
+            })
+        }
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <SignInForm buttonText='Log In'/>
+                {this.state.error ?
+                    <p style={{
+                        color: 'red',
+                    }}>
+                        Please try again with valid credentials!
+                    </p> : <div />}
+                <SignInForm buttonText='Log In' onSubmit={this.checkCredentials}/>
                 <div className='bottomText'>
                     Not a Price Tracker user yet?<br/>
                     <a href='#' onClick={this.props.routeTo.bind(this, 'register')}>
--- src/js/popup/components/SignInForm.jsx
+++ src/js/popup/components/SignInForm.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 
 export default class SignInForm extends Component {
+    state = {
+        email: "",
+        password: "",
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            email: "",
+            password: "",
+        })
+        this.props.onSubmit(this.state.email, this.state.password)
+    }
+
     render() {
         return (
             <React.Fragment>
-                <form className='loginType'>
-                    <input type="text" id="email" name="email" placeholder="Email" required="true"/><br/>
-                    <input type="password" id="password" name="password" placeholder="Password" required="true"/><br/>
+                <form className='loginType' onSubmit={this.onSubmit}>
+                    <input type="text" id="email" name="email" onChange={this.onChange}
+                        placeholder="Email" required="true" value={this.state.email}/><br />
+                    <input type="password" id="password" name="password" onChange={this.onChange}
+                        placeholder="Password" required="true" value={this.state.password}/><br />
                     <button>{this.props.buttonText}</button>
                 </form>
             </React.Fragment>
