11564e4210cc4bbbefa347654aa82ce3b779863f
Arjun Patel <amp490@rutgers.edu>
2020-11-30 22:12:53 -0500

    for mock

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials, firestore
+
 
 app = Flask(__name__)
 CORS(app)
+default_app = firebase_admin.initialize_app()
+db = firestore.client()
 
 from app import routes
