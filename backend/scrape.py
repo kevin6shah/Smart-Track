70df6ea65af3a88f777741cd1f61dc736d19346f
Kevin Shah <kevin6shah@gmail.com>
2020-12-17 04:32:17 -0500

    working in headless

--- backend/scrape.py
+++ backend/scrape.py
@@ -67,11 +67,13 @@ def init_webdriver():
     '''
     log_path = os.environ.get("LOG_FILE") + "webdriver.log"
     driver_path = os.environ.get("WEBDRIVER")
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 
     options = webdriver.ChromeOptions()
-    options.headless = False
+    options.headless = True
     prefs = {"profile.managed_default_content_settings.images": 2}
     options.add_experimental_option("prefs", prefs)
+    options.add_argument(f'user-agent={user_agent}')
 
     wd = webdriver.Chrome(
         executable_path=driver_path,
