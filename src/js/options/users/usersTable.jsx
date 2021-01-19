196c20af9a3740249cb49f46f70d3688ce60f2cd
Kevin Shah <kevin6shah@gmail.com>
2021-01-19 00:58:24 -0500

    delete user working

--- src/js/options/users/usersTable.jsx
+++ src/js/options/users/usersTable.jsx
@@ -43,6 +43,29 @@ class UsersTable extends Component {
         })
     }
 
+    onDelete = (row, i) => {
+        if (confirm("Are you sure that you want to delete this user?\nThis is IRREVERSIBLE!")) {
+            var db = firebase.firestore();
+            db.collection('users').doc(row[0]).get().then((snapshot) => {
+                const data = snapshot.data()
+                Object.keys(data['trackedMap']).forEach((ID) => {
+                    db.collection('items').doc(ID).get().then((itemSnapshot) => {
+                        let itemData = itemSnapshot.data()
+                        delete itemData['emailMap'][[row[2]]]
+                        db.collection('items').doc(ID).update({
+                            emailMap: itemData['emailMap']
+                        })
+                    })
+                })
+            })
+            db.collection('users').doc(row[0]).delete()
+            alert("User was deleted succesfully. Please manually delete them from the firebase auth")
+            let data = this.props.data
+            delete data[i]
+            this.setState({})
+        }
+    }
+
     render() {
         return (
             <div className="container">
@@ -59,7 +82,7 @@ class UsersTable extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                                {this.props.data.map((obj) => {
+                                {this.props.data.map((obj, i) => {
                                     return <tr key={obj[0]}>
                                         <th scope="row">{obj[0]}</th>
                                         <td>{obj[1]}</td>
@@ -76,7 +99,7 @@ class UsersTable extends Component {
                                         <td>
                                             <button type="button" className="btn btn-success" onClick={this.onEdit.bind(this, obj)}>
                                                 <i className={"fa" + (this.state.editing && this.state.selected === obj[0]? " fa-check" : "s fa-edit")}></i></button>
-                                            <button type="button" className="btn btn-danger"><i className="far fa-trash-alt"></i></button>
+                                            <button type="button" className="btn btn-danger" onClick={this.onDelete.bind(this, obj, i)}><i className="far fa-trash-alt"></i></button>
                                         </td>
                                     </tr>
                                 })
